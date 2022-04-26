843638c2aff39c981f5d9b7ac2acbedcad203883
Sima Nerush <2002ssn@gmail.com>
2022-04-26 11:26:33 -0700

    Smol Update

--- DailyThoughts/AuthenticationModel.swift
+++ DailyThoughts/AuthenticationModel.swift
@@ -40,30 +40,17 @@ final class AuthenticationModel: ObservableObject {
     }
   }
 
-  func listenToAuthState() {
-    Auth.auth().addStateDidChangeListener { [weak self] _, user in
-      guard let self = self else {
-        return
-      }
-      self.user = user
-    }
-  }
+  func listenToAuthState() {}
 
   func signUp(
     emailAddress: String,
     password: String
-  ) {
-    Auth.auth().createUser(withEmail: emailAddress, password: password)
-  }
+  ) {}
 
   func signIn(
     emailAddress: String,
     password: String
-  ) {
-    Auth.auth().signIn(withEmail: emailAddress, password: password)
-  }
+  ) {}
 
-  func signOut() {
-    try? Auth.auth().signOut()
-  }
+  func signOut() {}
 }
